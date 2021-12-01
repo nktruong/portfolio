a3490761977b6dc67eb8bb3a4a5912b11d77e17a
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-12-01 09:16:13 -0500

    current build of navbar (not completely working but its showing up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css'
 import Footer from './components/Footer'
-import Hero from './components/Hero'
+import Home from './pages/Home'
+import About from './pages/About'
+import Featured from './pages/Featured'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
 import Hotbar from './components/Hotbar'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Hotbar />
-      <Hero />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/featured" component={Featured} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+      </Routes>
       <Footer />
     </>
   )
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 function About(){
   return (
-    <div>
+    <div className="h-screen flex justify-center items-center bg-purple-100">
       <h1 className="text-4xl font-semibold bg-indigo-600">You've landed on the about page.</h1>
     </div>
   );
