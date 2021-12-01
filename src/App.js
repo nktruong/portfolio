5d3ee06475617ae4a148e5c24b15d5d63dc82034
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-12-01 08:20:00 -0500

    added navbar mobile/desktop

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css'
-import Home from './Home'
-import About from './components/About'
-import Featured from './Featured'
-import Portfolio from './Portfolio'
-import Contact from './Contact'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from "react-router-dom";
-import Hotbar from './hotbar'
+import Hotbar from './components/Hotbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl text-indigo-600">Hello TailwindCSS</h1>
-        <Hotbar />
-        {/* <Router to="/" component="{Home}"/> */}
-        <Router>
-          <nav>
-            <Link to="/" component={Home}>Home</Link>
-            <Link to="/About" component={About}>About</Link>
-            <Link to="/featured" component={Featured}>Featured</Link>
-            <Link to="/portfolio" component={Portfolio}>Portfolio</Link>
-            <Link to="/contact" component={Contact}>Contact</Link>
-          </nav>
-        </Router>
-    </div>
+    <>
+      <Hotbar/>
+    </>
   )
 }
 
--- src/components/Hotbar.js
+++ src/components/Hotbar.js
@@ -1,12 +1,22 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Hotbar(){
   return(
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about" >About</Link></li>
-      <li><Link to="/portfolio">Portfolio</Link></li>
-    </ul>
+    <nav className="flex justify-around items-center h-16 text-4xl font-semibold text-indigo-600 relative" role="navigation">
+      <div className="div px-4 cursor-pointer md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </div>
+      <div className="pr-8 hidden md:block">
+        <Link className="p-4" to="/">Home</Link>
+        <Link className="p-4" to="/about">About</Link>
+        <Link className="p-4" to="/featured">Featured</Link>
+        <Link className="p-4" to="/portfolio">Portfolio</Link>
+        <Link className="p-4" to="/contact">Contact</Link>
+      </div>
+    </nav>
   );
 }
 
