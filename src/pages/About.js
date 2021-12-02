b2fd71149692d7acdcd7e4dfa70b8d12f1c91354
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-12-02 00:25:19 -0500

    Added info to About component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,10 @@
 function About(){
   return (
-    <div className="">
-      <h1 className="text-4xl text-left font-semibold text-indigo-600 pt-20">Who Am I?</h1>
+    <div className="max-w-5xl mx-auto animate-fade-in-down">
+      <h1 className="lg:text-6xl text-4xl font-semibold dark:text-transparent bg-clip-text caret-pink-600 bg-gradient-to-br dark:from-purple-500 dark:via-purple-400 dark:to-fuchsia-100 pt-20 pb-10">Who am I?</h1>
+      <p className="text-2xl dark:text-gray-50 leading-relaxed">I play a lot of Minecraft and also do full-stack software development. I have been working at <span className="text-red-500">Resilient Coders</span> for the past couple of months as a Software Engineer. I've worked design gigs in the past like at MDA Let's Play being a freelance graphic designer. So naturally, when learning to be a developer I stumbled upon a treasure trove once I understood CSS.</p>
+      <h2 className="lg:text-4xl dark:text-indigo-200 font-semibold my-8">My Tech Stack</h2>
+      <p className="lg:text-2xl dark:text-gray-50 font-light">React, EJS, Node.js, Express, MongoDB, TailwindCSS, CSS3, and HTML5.</p>
     </div>
   );
 }
