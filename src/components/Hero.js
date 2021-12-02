353dff00b3a7a7d91a113014924dae2def83d0e0
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-12-02 10:47:01 -0500

    Added dark mode to Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className="flex justify-around max-w-7xl m-auto py-40 animate-fade-in-down">
       <div className="w-1/2 text-left pr-6">
-        <h1 className="lg:text-5xl md:text-3xl sm:text-xl text-2xl text-green-900 dark:text-transparent bg-clip-text caret-pink-600 bg-gradient-to-br dark:from-purple-500 dark:via-purple-400 dark:to-fuchsia-100 font-bold mb-12">Welcome to my site! 
+        <h1 className="lg:text-5xl md:text-3xl sm:text-xl text-2xl bg-gradient-to-br from-green-900 via-green-700 to-yellow-300 text-transparent bg-clip-text caret-pink-600 dark:from-purple-500 dark:via-purple-400 dark:to-fuchsia-100 font-bold mb-12">Welcome to my site! 
         <span class="block">I'm Nathan.</span></h1>
         <p className="lg:text-2xl font-medium text-green-800 dark:text-indigo-200 tracking-tight">It's nice to meet you! You can roam through this site to see who I am, what I do, what projects I've done, or you can check out my socials.</p>
 
@@ -15,11 +15,11 @@ function Hero() {
               <div className="absolute -inset-0.5 bg-gradient-to-r from-green-500 to-yellow-300 dark:from-pink-600 dark:to-purple-600 rounded-lg filter blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
               <button>
               <Link className="relative py-3 px-4 bg-gray-800 rounded-xl shadow-md text-xl text-gray-50 font-semibold transition duration-150 ease-in flex items-center space-x-5 w-full" to="/featured">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-4 text-fuchsia-500 -rotate-3 transform" fill="" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-4 text-green-500 dark:text-fuchsia-500 -rotate-3 transform" fill="" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
                 </svg>
                 <span>
-                Check out my latest project, <span className="text-yellow-300 dark:text-cyan-400 group-hover:text-gray-100 transition duration-200"> Cookdeck
+                Check out my latest project, <span className="text-yellow-300 dark:text-cyan-400 group-hover:text-gray-100 transition duration-200"> Cook Deck
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline transform group-hover:translate-x-1.5 duration-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
                 </svg>
@@ -34,7 +34,10 @@ function Hero() {
       </div>
       <div className="w-1/2">
         <Link to="/bityard">
+        <picture>
           <img className="transform -rotate-3 rounded-lg shadow-inner scale-90 hover:scale-95 transition ease-in duration-150 cursor-pointer" src="https://media.discordapp.net/attachments/763423276671434812/915047670530310144/2021-11-29_20.12.32.png?width=2160&height=1162" alt="Minecraft screenshot of green hills and white horses"/>
+          <source media="(prefers-color-scheme: dark)" className="transform -rotate-3 rounded-lg shadow-inner scale-90 hover:scale-95 transition ease-in duration-150 cursor-pointer" srcset="https://resourcepack.net/fl/images/2020/08/BSL-Shaders-for-minecraft-8.jpg" alt="Minecraft screenshot of the Nether with shaders"/>
+        </picture>
         </Link>
       </div>
     </div>
