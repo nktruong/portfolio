ce4deb925dd5cb2a59bf2d4b751ea260278f9e95
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-12-02 00:24:59 -0500

    Added fade in down animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,21 @@ module.exports = {
         fuchsia: colors.fuchsia,
         yellow: colors.amber,
         blue: colors.blue
+      },
+      keyframes: {
+          'fade-in-down': {
+              '0%': {
+                  opacity: '0',
+                  transform: 'translateY(-10px)'
+              },
+              '100%': {
+                  opacity: '1',
+                  transform: 'translateY(0)'
+              },
+          }
+      },
+      animation: {
+          'fade-in-down': 'fade-in-down 0.5s ease-out'
       }
     },
   },
