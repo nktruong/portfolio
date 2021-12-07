699b6d56a80994d1fa83656c6b42bb8b88b0bd5b
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2021-12-07 16:50:41 -0500

    Added children props

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,7 @@
-function Container() {
+function Container( {children} ) {
   return (
     <div className="max-w-5xl pb-48 mx-auto animate-fade-in-down font-dm ">
+      {children}
     </div>
   )
 }
